a641c65fdc992d3c8bc1623b52231dd056194def
Abdul-Muqaddam <chandansar444999@gmail.com>
2024-11-14 19:45:17 +0500

    initail commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      Hey how are you this is Abdul-Muqaddam here and this is the first initials comment
+    </div>
+    </>
+    
+  );
+}
